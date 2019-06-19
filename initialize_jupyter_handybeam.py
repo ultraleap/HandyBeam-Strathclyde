4c1c71ee35278ca1b8e0639f234a3198f8662543
Jerzy Dziewierz <jerzy.dziewierz@ultrahaptics.com>
2019-06-19 18:31:20 +0100

    remove root folder to increase security add scipy for optimisation and windows

--- initialize_jupyter_handybeam.py
+++ initialize_jupyter_handybeam.py
@@ -9,7 +9,6 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
-sys.path.append("../..")
 sys.path.append("../handybeam_core_repo/")
 
 import handybeam
@@ -18,7 +17,10 @@ import handybeam.tx_array_library
 import handybeam.tx_array
 import handybeam.visualise
 import handybeam.samplers.rectilinear_sampler
+import handybeam.samplers.clist_sampler
 from handybeam.solver import Solver
 # matplotlib.rcParams['figure.figsize'] = [15, 10]
 from handybeam.misc import HandyDict
+import scipy.signal.windows
+
 import strathclyde
